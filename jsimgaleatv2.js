173cce5b21fbbc59d4df79b15cfdbdf0339f3770
yusfer <yustasfernando@gmail.com>
2015-02-03 13:14:16 +0100

    Finalizada la versión 2

--- jsimgaleatv2.js
+++ jsimgaleatv2.js
@@ -1,16 +1,8 @@
-
-	urlimages = ["http://cadenaser00.epimg.net/ser/imagenes/2014/06/25/cultura/1403651828_740215_0000000000_noticia_normal.jpg",
-					"http://lopezlinares.com/vintageblog/wp-content/uploads/2014/01/casablanca-15.jpg",
-					"https://alquimistacinefilo.files.wordpress.com/2013/05/clasico-elpadrino.jpg",
-					"https://ekonomicus.files.wordpress.com/2014/11/banksters1.jpg",
-					"http://culturacolectiva.com/wp-content/uploads/2014/03/110.jpg",
-					"http://mexico.cnn.com/media/2012/01/03/pjaros-hitchcock.jpg"];
 					
 					
 	function changerImg (element,max) {
 	
 	var aleat = (Math.round(Math.random()*(max-1)))*2 + 1 //calcula aleatorio entre 0 y 7, lo multiplica por 2 y suma uno porque childNodes los impares
-	alert(aleat)
 	element.childNodes[aleat].attributes[0].nodeValue = "display:inherit"
 	
 	}
